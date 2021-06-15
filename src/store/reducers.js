2d91afe8d92051581f1bb84f7165249b65432880
FIZZO PANNOSCH <fizzo999@gmail.com>
2021-06-14 17:18:56 -0700

    implemented API data from online

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,19 +1,18 @@
 let initialState = {
   products: [],
-  count: 0,
-  filteredProducts: [],
-  data: [1, 2, 3],
+  // count: 0,
+  // filteredProducts: [],
+  // data: [1, 2, 3],
 };
 
 export default (state = initialState, action) => {
   const { type, payload } = action;
   // let newResults = [];
+  console.log('here is the state in the reducer', state);
   switch(type) {
   case 'GET_ONLINE_PRODUCTS':
-    // return { results: [..., payload ]};
-    // newResults = state.results
-    // return newResults.push(payload);
-    return { products: payload.results, count: payload.count };
+    console.log('we are in online GET PRODUCTS and here is body', payload.body);
+    return { products: payload.body.products};
   case 'POST_ONLINE_PRODUCTS':
     return 'SOMETHING';
   case 'PUT_ONLINE_PRODUCTS':
